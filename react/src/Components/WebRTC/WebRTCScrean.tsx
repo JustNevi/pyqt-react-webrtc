7540fe157f28d9cdbe259b5a56677800bd71afae
JustNevi <slavkad25@gmail.com>
2025-07-04 18:03:24 +0200

    chore(react): create start components

---
+++ react/src/Components/WebRTC/WebRTCScrean.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WebRTCScrean() {
+  return <div>WebRTCScrean</div>;
+}
+
+export default WebRTCScrean;
---
+++ react/src/Components/WebRTC/WebRTCView.tsx
@@ -0,0 +1,37 @@
+import WebRTCScrean from "./WebRTCScrean";
+
+function WebRTCView() {
+  return (
+    <>
+      <WebRTCScrean></WebRTCScrean>
+      <div className="input-group mb-3">
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addon1"
+        >
+          Create offer
+        </button>
+        <textarea
+          className="form-control"
+          aria-label="With textarea"
+        ></textarea>
+      </div>
+      <div className="input-group mb-3">
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addon1"
+        >
+          Create answer
+        </button>
+        <textarea
+          className="form-control"
+          aria-label="With textarea"
+        ></textarea>
+      </div>
+    </>
+  );
+}
+
+export default WebRTCView;
