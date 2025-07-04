b40c7db732f4d777576a2576a9dd537d9c1212af
JustNevi <slavkad25@gmail.com>
2025-07-04 18:17:36 +0200

    chore(react): return to App.tsx

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-
-import WebRTCView from "./Components/WebRTC/WebRTCView.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WebRTCView />
+    <App />
   </StrictMode>
 );
