6943251b84199160ac938a2f1fd34c1315d34a19
JustNevi <slavkad25@gmail.com>
2025-07-06 22:14:37 +0200

    chore(signal): add url patterns to api views

--- signal/django/rtc_signaling/api_v1/urls.py
+++ signal/django/rtc_signaling/api_v1/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('add-offer-sd/', views.addOfferSessionDescription, name='add_offer_sd'),
+    path('add-answer-sd/', views.addAnswerSessionDescription, name='add_answer_sd'),
+    path('get-offer-sd/', views.getOfferSessionDescription, name='get_offer_sd'),
+    path('get-answer-sd/', views.getAnswerSessionDescription, name='get_answer_sd'),
+    path('add-ice-candidate/', views.addIceCandidate, name='add_ice_candidate'),
+    path('get-offer-ice-candidates/', views.getOfferIceCandidates, name='get_offer_ice_candidates'),
+    path('get-answer-candidates/', views.getAnswerCandidates, name='get_answer_candidates'),
 ]
